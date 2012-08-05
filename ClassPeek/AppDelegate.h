6f9f559632893bd334cc721935d194da0d04953a
Andrew Rauh <andrew@andrewrauh.com>
2012-08-04 19:41:20 -0700

    first commit

---
+++ ClassPeek/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassPeek
+//
+//  Created by Andrew Rauh on 8/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClassPeek/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ClassPeek
+//
+//  Created by Andrew Rauh on 8/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
